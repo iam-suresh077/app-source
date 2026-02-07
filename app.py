39b9f7e74c66f2516213622ed0e8d0e8dcd9e510
iam-suresh077 <129142479+iam-suresh077@users.noreply.github.com>
2026-02-07 16:47:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from GitOps 🚀"
+    return "Hello World from GitOps 🚀 | hey it's working"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
